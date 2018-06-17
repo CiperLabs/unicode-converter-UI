2a1ab0008f7807d77d6d13dad480dd18acd5ac26
gayansandaruwan <gayankavirathne.14@cse.mrt.ac.lk>
2018-06-17 21:29:14 +0530

    Fixed a bug in a URL

--- src/api-config.js
+++ src/api-config.js
@@ -6,4 +6,4 @@ const hostname = window && window.location && window.location.hostname;
 
   backendHost = process.env.REACT_APP_BACKEND_HOST || 'http://localhost:8080';
 
-export const API_ROOT = `${backendHost}/api/${apiVersion}`;
+export const API_ROOT = `${backendHost}`;
